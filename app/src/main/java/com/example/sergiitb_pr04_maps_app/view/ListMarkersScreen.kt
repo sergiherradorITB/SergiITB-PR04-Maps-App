d453e7c7b2a74a00cb1f9f53d976eff2d5d43811
sergi.herrador.7e7 <sergi.herrador.7e7@itb.cat>
2024-03-19 13:56:08 +0100

    Todos con el mismo tamaño

--- app/src/main/java/com/example/sergiitb_pr04_maps_app/view/ListMarkersScreen.kt
+++ app/src/main/java/com/example/sergiitb_pr04_maps_app/view/ListMarkersScreen.kt
@@ -2,6 +2,8 @@ package com.example.sergiitb_pr04_maps_app.view
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,10 +11,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Card
@@ -31,8 +35,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -134,9 +140,11 @@ fun LocationItem(
             Image(
                 bitmap = marker.photo.asImageBitmap(),
                 contentDescription = "itb-defaultLogo",
+                contentScale = ContentScale.FillBounds, // no se si dejarlo pero bueno
                 modifier = Modifier
                     .fillMaxSize()
                     .alpha(0.4f) // Ajusta la transparencia de la imagen
+                    .size(250.dp)
                     //.background(marker.category.colorResId)
                     .clickable {
                         mapViewModel.changePosition(marker.position)
